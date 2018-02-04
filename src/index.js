69952022b8802ebb6a3b00541dd8c3d718a7d380
Jacob Hoke <jacob@threads13.com>
2018-02-04 16:04:46 -0500

    inserted main part of app.  push to Heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/video_list"
+import VideoDetail from "./components/video_detail";
+import _ from "lodash";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const API_KEY = "AIzaSyBy8s85VocirJP2Aw2VBa9qbuKUwG3Qn0c";
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch("cheese");
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+		return (
+			<div>
+				<h1>React Youtuber</h1>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+  <App />
+  ,document.getElementById('root'));
 registerServiceWorker();
